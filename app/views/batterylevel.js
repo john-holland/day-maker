b307bb99aca6b464d005f554adfcbd198e5720d2
John Holland <jholland87@gmail.com>
2018-04-14 08:18:49 -0400

    (hopefully) fixed the battery estimate, added a license, and added resolution specific styles for ionic / versa uis.

--- app/views/batterylevel.js
+++ app/views/batterylevel.js
@@ -10,11 +10,6 @@ let document = require("document");
 
 const $ = $at('#batterylevel');
 
-let batteryEstimate = {
-  charge: undefined,
-  time: undefined
-}
-
 //for calc of moving average
 let durationPerPercentChargeData = []
 
@@ -47,52 +42,100 @@ function average(numbers, maxlen) {
   
   return sum / numbers.length
 }
+const SECONDS_FOR_AVG_BUFFER_RESET = 60 * 60
+const ESTIMATE_INTERVAL = 30000
+const DATA_BUFFER_SIZE = 60 //last half hour
+const DATA_BUFFER_ESTIMATE_MINIMUM = 10 //5 minutes of readings
+
+class BatteryEstimator {
+  data = []
+  estimateIntervalId = undefined
+  batteryEstimate = {
+    charge: undefined,
+    time: undefined
+  }
+
+  currentTimeInSeconds() {
+    return (new Date()).getTime() / 1000
+  }
+
+  reset() {
+    this.stop()
+    this.start()
+  }
+
+  start() {
+    this.batteryEstimate.charge = battery.charge
+    this.batteryEstimate.time = this.currentTimeInSeconds()
+    
+    this.estimateIntervalId = setInterval(function() {
+      let currentTimeInSeconds = this.currentTimeInSeconds()
+      let diffSeconds = currentTimeInSeconds - this.batteryEstimate.time
+      
+      //it should reset if they charge anyways, but just in case
+      if (diffSeconds > SECONDS_FOR_AVG_BUFFER_RESET || this.shouldReset()) {
+        this.reset()
+        return
+      }
+
+      //then the estimate will be the change in charge / diffSeconds
+      let batterydiff = this.batteryEstimate.charge - battery.charge
+
+      let durationPerPercentCharge = diffSeconds / batterydiff
+      this.data.push(durationPerPercentCharge)
+
+      this.batteryEstimate.time = currentTimeInSeconds
+      this.batteryEstimate.charge = battery.charge
+    }.bind(this), ESTIMATE_INTERVAL)
+  }
+
+  isEstimateAvailable() {
+    return this.batteryEstimate.charge >= battery.charge && this.data.length >= DATA_BUFFER_ESTIMATE_MINIMUM
+  }
+
+  getEstimate() {
+    return average(this.data, DATA_BUFFER_SIZE) * battery.charge
+  }
+
+  shouldReset() {
+    return this.batteryEstimate.charge < battery.charge || charger.connected
+  }
+
+  stop() {
+    this.data = []
+    clearInterval(this.estimateIntervalId)
+    this.estimateIntervalId = undefined
+    this.batteryEstimate.charge = undefined
+    this.batteryEstimate.time = undefinedc
+  }
+}
 
 export class BatteryLevelUI extends UserInterface {
   name = 'batterylevel'
+  batteryEstimator = new BatteryEstimator()
 
   constructor() {
     super()
     this.$ = $
     this.el = this.$()
+    this.batteryEstimator.start()
   }
 
 	onRender() {
     super.onRender()
     this.$("#time").text = `${battery.chargeLevel || 0}% power`
     const SECONDS_FOR_ESTIMATE = 30
-    const SECONDS_FOR_AVG_BUFFER_RESET = 60 * 60
-    let currentTimeSeconds = (new Date()).getTime() / 1000
+    
     let chargeLevel = battery.chargeLevel
     const HOURS = 24 * 4
       
     let estimate = createEstimateMessage(HOURS * (chargeLevel / 100))
-    if (batteryEstimate.charge === undefined || batteryEstimate.charge < chargeLevel || charger.connected) {
+    if (this.batteryEstimator.shouldReset()) {
       //if we don't yet have a suitable charge / time
       //if we charged since the last time we checked or we're currently charging, reset the estimate
-      batteryEstimate.charge = chargeLevel
-      batteryEstimate.time = currentTimeSeconds
-      durationPerPercentChargeData = []
-    } else if (batteryEstimate.charge > chargeLevel) {
-      let diffSeconds = currentTimeSeconds - batteryEstimate.time
-      if (diffSeconds > SECONDS_FOR_ESTIMATE) {
-        //we don't want to use the rolling average if it's been more than an hour(?)
-        if (diffSeconds > SECONDS_FOR_AVG_BUFFER_RESET) {
-          durationPerPercentChargeData = []
-        }
-        
-        //then the estimate will be the change in charge / diffSeconds
-        let batterydiff = batteryEstimate.charge - chargeLevel
-        
-        let durationPerPercentCharge = diffSeconds / batterydiff
-        durationPerPercentChargeData.push(durationPerPercentCharge)
-        
-        console.log(JSON.stringify(durationPerPercentChargeData))
-        
-        let estimateSeconds = average(durationPerPercentChargeData, 5) * chargeLevel
-        
-        estimate = createEstimateMessage(estimateSeconds / 60 / 60)
-      }      
+      this.batteryEstimator.reset()
+    } else if (this.batteryEstimator.isEstimateAvailable()) {
+      estimate = createEstimateMessage(estimateSeconds / 60 / 60)
     }
     
     this.$("#batteryestimate").text = estimate
--- readme.md
+++ readme.md
@@ -9,10 +9,37 @@ The watchface features configurable heartrate display and the ability to turn of
 
 The two bars at the bottom are active minutes and a battery indicator.
 
-Uses a slightly modified version of the wonderful [ionic views library](https://github.com/gaperton/ionic-views) to switch between stats screens.
-
 Optionally pulls a random image of the sunrise from [unsplash.com](https://unsplash.com/) to show while you are waking up!
 
 Optionally floats the fitbit logo across the screen - count them and fall asleep faster!
 
 Optionally change the font / bar colors at night to be easier on the eyes if it accidentally (or intentionally) turns on.
+
+Attributions
+=======
+
+Uses a slightly modified version of the wonderful [ionic views library](https://github.com/gaperton/ionic-views) to switch between stats screens.
+
+License
+=======
+MIT License
+
+Copyright (c) 2018 John Holland
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
